975f5d18363f7a5bd1508e17ab4e06473e484b28
@ShriTaralkar <shripad.taralkar@gmail.com>
2023-11-02 19:26:20 +1100

    Updated the content

--- src/components/Purpose.jsx
+++ src/components/Purpose.jsx
@@ -7,11 +7,10 @@ function Purpose({ header, subheader, body, imgsrc }) {
     return (
         <section className="m-auto p-8 text-center">
             <header className="m-3">
-                <h2 className="text-5xl font-bold">Purpose</h2>
-                <h3 className="m-1 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quia?</h3>
+                <h2 className="text-5xl font-bold">Our Mission</h2>
             </header>
             <div className="md:w-1/2 m-auto p-2 flex flex-col items-center leading-8">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, temporibus fuga numquam similique, dolorum veniam voluptatum cum consequuntur magni quisquam unde, sapiente nam veritatis pariatur ab rem ducimus ex quaerat recusandae exercitationem! Asperiores, autem vero dolore facere magnam sit laboriosam incidunt placeat error quia, inventore ut vitae tenetur dolorem eos.</p>
+                <p>"To kindle the leadership flame with humility, empathy, and skill, fostering environments of thriving, inclusivity, and unwavering purpose, sparking success and personal growth for every individual."</p>
                 <span className="m-1 w-1/2 md:w-1/3 flex justify-evenly text-3xl text-gray-600 text-center">
                     <GiTeacher /><FaChartLine /><ImRocket />
                 </span>
