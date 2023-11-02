52488a29bf5182c9796289819bf63fe7877355d6
ShriTaralkar <144521822+ShriTaralkar@users.noreply.github.com>
2023-11-02 12:34:58 +1100

    Created a copy of the repo Client
    So that we can deploy to Netlify

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+	siteMetadata: {
+		title: `client`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		'gatsby-plugin-postcss',
+	],
+}
