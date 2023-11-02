27ca9939b4237fd92adb697139b293f3990277ce
@ShriTaralkar <shripad.taralkar@gmail.com>
2023-11-02 19:00:52 +1100

    Updated the button and text alingment to right

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,9 @@ function Hero() {
   return (
     <section className="md:h-screen md:px-3 flex flex-col items-center md:flex-row md:justify-between transition-all ">
         <div className="left max-w-1/2 flex flex-col justify-evenly items-center md:items-start p-4 text-center md:text-left">
-            <h1 className="text-6xl my-2 font-bold">"Leading with Humility = Fostering Excellence."</h1>
-            <p className="text-lg my-2"> Inspiring Leaders Transform Organisations. Traditional, authoritarian leadership styles are no longer as effective, and organisations are increasingly recognizing the value of humble leadership qualities..</p>
-            <button className="my-2 p-4 w-36 flex items-center justify-evenly bg-green-500 text-white capitalize rounded-full font-bold transition-all duration-300 hover:translate-x-2">Schedule a free discovery session<AiOutlineArrowRight /></button>
+            <h1 className="text-6xl my-2 font-bold">Leading with Humility = Fostering Excellence.</h1>
+            <p className="text-lg my-2 text-left">Inspiring Leaders Transform Organisations. Traditional, authoritarian leadership styles are no longer as effective, and organisations are increasingly recognizing the value of humble leadership qualities.</p>
+            <button className="my-2 p-4 w-48 flex items-center justify-evenly bg-sky-blue text-white capitalize rounded-full font-bold transition-all duration-300 hover:translate-x-2">Schedule a free discovery session<AiOutlineArrowRight /></button>
         </div>
         <div className="right flex max-w-1/2 p-4">
             <img src="https://img.freepik.com/free-vector/isometric-soft-skills-concept-icon-with-three-characters-listening-their-mentor_1284-63039.jpg?w=740&t=st=1696941746~exp=1696942346~hmac=9418a5b4523388a8e3300d4050a06c271a708b21af7e914f8b8e1e3b6a2671ace" alt="" className="img-fluid" />
