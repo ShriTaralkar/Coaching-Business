0e701243203d8687534c0167c45260c6015700e8
@ShriTaralkar <shripad.taralkar@gmail.com>
2023-11-03 13:30:22 +1100

    Still trying to fix the image height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,12 @@ function Hero() {
       <div className="left flex flex-col justify-evenly items-center md:items-start p-4 text-center md:text-left">
         <h1 className="text-6xl my-2 font-bold">Leading with Humility = Fostering Excellence.</h1>
         <p className="text-lg my-2 text-left">Inspiring Leaders Transform Organisations. Traditional, authoritarian leadership styles are no longer as effective, and organisations are increasingly recognizing the value of humble leadership qualities.</p>
-        <button className="my-2 p-4 w-48 flex items-center justify-evenly bg-sky-blue text-white capitalize rounded-full font-bold transition-all duration-300 hover:translate-x-2">Schedule a free discovery session<AiOutlineArrowRight /></button>
+        <button className="my-2 p-4 w-48 flex items-center justify-evenly bg-sky-blue text-grey capitalize rounded-full font-bold transition-all duration-300 hover:translate-x-2">Schedule a free discovery session<AiOutlineArrowRight /></button>
       </div>
       <div className="right flex flex-grow p-4">
-        <img className="img max-w-full h-full rounded-xl" src={HeroImage} alt="" />
+        <div className="content-container flex flex-col h-full">
+          <img className="img max-w-full flex-grow" src={HeroImage} alt="" className="img-fluid" />
+        </div>
       </div>
     </section>
   )
