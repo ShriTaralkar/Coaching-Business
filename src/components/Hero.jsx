10be176b6468ac1ab7643b35d7a6e45783c93484
@ShriTaralkar <shripad.taralkar@gmail.com>
2023-11-03 13:08:08 +1100

    adjusted height and width to same

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,14 @@ import HeroImage from "../images/Vertical_Doorway_Nature.jpg";
 
 function Hero() {
   return (
-    <section className="md:h-screen md:px-3 flex flex-col items-center md:flex-row md:justify-between transition-all ">
-      <div className="left max-w-1/2 flex flex-col justify-evenly items-center md:items-start p-4 text-center md:text-left">
+    <section className="md:h-screen md:px-3 flex flex-col items-center md:flex-row md:justify-between transition-all">
+      <div className="left flex flex-col justify-evenly items-center md:items-start p-4 text-center md:text-left">
         <h1 className="text-6xl my-2 font-bold">Leading with Humility = Fostering Excellence.</h1>
         <p className="text-lg my-2 text-left">Inspiring Leaders Transform Organisations. Traditional, authoritarian leadership styles are no longer as effective, and organisations are increasingly recognizing the value of humble leadership qualities.</p>
         <button className="my-2 p-4 w-48 flex items-center justify-evenly bg-sky-blue text-white capitalize rounded-full font-bold transition-all duration-300 hover:translate-x-2">Schedule a free discovery session<AiOutlineArrowRight /></button>
       </div>
-      <div className="right flex max-w-1/2 p-4">
-        <img className="img max-w-full rounded-xl" src={HeroImage} alt="" />
+      <div className="right flex flex-grow p-4">
+        <img className="img max-w-full h-full rounded-xl" src={HeroImage} alt="" />
       </div>
     </section>
   )
